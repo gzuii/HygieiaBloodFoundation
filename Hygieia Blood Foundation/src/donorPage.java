708505c227497113043da994bbf776628fbfcc74
chippee08 <93817250+chippee08@users.noreply.github.com>
2022-11-29 19:26:54 +0800

    Update donorPage.java

--- Hygieia Blood Foundation/src/donorPage.java
+++ Hygieia Blood Foundation/src/donorPage.java
@@ -12,15 +12,10 @@ import javax.swing.JTextField;
 
 public class donorPage implements ActionListener{
 
-	//JFrame declaration
 	JFrame frame = new JFrame();
-	
-	//button declaration
 	JButton backButton = new JButton();
 	ImageIcon image = new ImageIcon("company_logo.png");
 	ImageIcon logo = new ImageIcon("hygieia_logo.png");
-	
-	//textfield declaration
 	JTextField firstNameField = new JTextField();
 	JTextField lastNameField = new JTextField();
 	JTextField middleNameField = new JTextField();
@@ -28,11 +23,6 @@ public class donorPage implements ActionListener{
 	JTextField sex = new JTextField();
 	JTextField address = new JTextField();
 	JTextField contact_num = new JTextField();
-	
-	//combobox for the dropdown list
-	JComboBox vID = new JComboBox();
-	
-	//JLabel declaration
 	JLabel firstnameLabel = new JLabel("First name:");
 	JLabel lastnameLabel = new JLabel("Surname:");
 	JLabel middlenameLabel = new JLabel("Middle Name:");
@@ -41,31 +31,61 @@ public class donorPage implements ActionListener{
 	JLabel add = new JLabel("Address:");
 	JLabel contact = new JLabel("Contact number:");
 	JLabel ID = new JLabel("Valid ID:");
+	JLabel personal_info = new JLabel("Personal Information");
 	
-	//constructor
+	String[] IDs = {"<Select an option>", "National ID", "Driver's License", "Passport", "Senior Citezen ID", "Student ID"};
+	JComboBox vID = new JComboBox(IDs);
+	
+	
 	donorPage(){
 		
-		//firstname settings
+		personal_info.setBounds(150, 90, 250, 35);
+		personal_info.setFont(new Font(null, Font.PLAIN, 25));
+		
 		firstnameLabel.setBounds(100, 145, 250, 35);
 		firstnameLabel.setFont(new Font(null, Font.PLAIN, 15));
 		firstNameField.setBounds(222, 150, 200, 25);
 		
-		//lastname settings
 		lastnameLabel.setBounds(100, 175, 250, 35);
 		lastnameLabel.setFont(new Font(null, Font.PLAIN, 15));
 		lastNameField.setBounds(222, 180, 200, 25);
-
-		//middlename settings
+		
 		middlenameLabel.setBounds(100, 205, 250, 35);
 		middlenameLabel.setFont(new Font(null, Font.PLAIN, 15));
 		middleNameField.setBounds(222, 210, 200, 25);
 		
-		//age settings 
 		age_text.setBounds(100, 235, 250, 35);
 		age_text.setFont(new Font(null, Font.PLAIN, 15));
 		age.setBounds(222, 240, 200, 25);
 		
-		//frame settings
+		sex_text.setBounds(100, 265, 250, 35);
+		sex_text.setFont(new Font(null, Font.PLAIN, 15));
+		sex.setBounds(222, 270, 200, 25);
+		
+		add.setBounds(100, 295, 250, 35);
+		add.setFont(new Font(null, Font.PLAIN, 15));
+		address.setBounds(222, 300, 200, 25);
+		
+		contact.setBounds(100, 325, 250, 35);
+		contact.setFont(new Font(null, Font.PLAIN, 15));
+		contact_num.setBounds(222, 330, 200, 25);
+		
+		ID.setBounds(100, 355, 250, 35);
+		ID.setFont(new Font(null, Font.PLAIN, 15));
+		vID.setBounds(222, 360, 200, 25);
+		vID.addActionListener(this);
+		
+		
+		
+		frame.add(personal_info);
+		frame.add(ID);
+		frame.add(vID);
+		frame.add(contact);
+		frame.add(contact_num);
+		frame.add(address);
+		frame.add(add);
+		frame.add(sex);
+		frame.add(sex_text);
 		frame.add(age_text);
 		frame.add(age);
 		frame.add(middleNameField);
